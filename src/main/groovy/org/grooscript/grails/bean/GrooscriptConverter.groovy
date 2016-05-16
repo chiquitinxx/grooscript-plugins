93a929794a82fb33416f0416d3c7473ca8b7ddc7
Christian Meyer <christian.meyer@gmail.com>
2016-05-16 13:30:46 +0200

    Minor cosmetic changes.

--- src/main/groovy/org/grooscript/grails/bean/GrooscriptConverter.groovy
+++ src/main/groovy/org/grooscript/grails/bean/GrooscriptConverter.groovy
@@ -1,6 +1,8 @@
 package org.grooscript.grails.bean
 
 import grails.plugin.cache.Cacheable
+import org.codehaus.groovy.control.CompilerConfiguration
+import org.codehaus.groovy.control.customizers.ASTTransformationCustomizer
 import org.grooscript.GrooScript
 import org.grooscript.convert.ConversionOptions
 import org.grooscript.grails.component.Component
@@ -8,6 +10,7 @@ import org.grooscript.grails.remote.RemoteDomainClass
 
 import javax.annotation.ParametersAreNonnullByDefault
 
+import static org.codehaus.groovy.control.customizers.builder.CompilerCustomizationBuilder.withConfig
 import static org.grooscript.grails.util.Util.*
 
 /**
@@ -24,12 +27,11 @@ class GrooscriptConverter {
 
     @Cacheable('conversions')
     String toJavascript(String groovyCode, Map conversionOptions = null) {
-        if (!groovyCode) {
-            return ''
-        }
+        if (!groovyCode)
+            ''
 
         try {
-            GrooScript.convert(groovyCode, addDefaultOptions(conversionOptions))
+            GrooScript.convert(groovyCode, addDefaultOptions(conversionOptions ?: [:]))
         } catch (e) {
             consoleError "Error converting to javascript: ${e.message}"
             ''
@@ -56,38 +58,34 @@ class GrooscriptConverter {
         conversionOptions[ConversionOptions.CLASSPATH.text] = GROOVY_SRC_DIR
         conversionOptions[ConversionOptions.INCLUDE_DEPENDENCIES.text] = true
         conversionOptions[ConversionOptions.CUSTOMIZATION.text] = {
-            ast(Component)
+            withConfig(new CompilerConfiguration()) {
+                ast(Component)
+            }
         }
         toJavascript(groovyCode, addScopeVars(conversionOptions))
     }
 
     private static Map addDefaultOptions(Map options) {
-        options = options ?: [:]
-        options = addGroovySourceClassPathIfNeeded(options)
-        options = addScopeVars(options)
-        options
+        if (!options.classpath)
+            options.classpath = []
+        else {
+            if (options.classpath instanceof String)
+                options.classpath = [options.classpath]
+        }
+
+        if (!options.classpath.contains(GROOVY_SRC_DIR))
+            options.classpath << GROOVY_SRC_DIR
+
+        addScopeVars(options)
     }
 
     private static Map addScopeVars(Map options) {
-        if (!options.mainContextScope) {
+        if (!options.mainContextScope)
             options.mainContextScope = []
-        }
+
         options.mainContextScope.addAll DEFAULT_CONVERSION_SCOPE_VARS
         options
     }
 
-    private static Map addGroovySourceClassPathIfNeeded(Map options) {
-        if (!options.classpath) {
-            options.classpath = []
-        } else {
-            if (options.classpath instanceof String) {
-                options.classpath = [options.classpath]
-            }
-        }
-        if (!options.classpath.contains(GROOVY_SRC_DIR)) {
-            options.classpath << GROOVY_SRC_DIR
-        }
-        options
-    }
 
 }
